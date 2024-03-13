ca1497c6ad52cb6b84fa0bb0ad9df1f83413fc01
work2win <worknpeace2win@gmail.com>
2024-03-13 15:22:27 -0400

    commits

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'company-angular'`, () => {
+  it(`should have as title 'Angularjobsearch'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('company-angular');
+    expect(app.title).toEqual('Angularjobsearch');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, company-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angularjobsearch');
   });
 });
